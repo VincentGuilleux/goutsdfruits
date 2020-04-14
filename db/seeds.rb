07950341f3a21b32132da07837726788b2e44776
sebastien etienne <sebastienetienne.pro@gmail.com>
2020-04-14 18:50:21 +0200

    seeds and pictures functionnal - first draft of seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
  require "open-uri"
 
-Client.destroy_all
+OrderLineProductLot.destroy_all
+ProductLot.destroy_all
+OrderLine.destroy_all
 Order.destroy_all
+Client.destroy_all
+Product.destroy_all
 User.destroy_all
-#Order_line.destroy_all
-# Order_line_product_lot.destroy_all
-# Product.destroy_all
-# Product_lot.destroy_all
 
 
 # This file should contain all the record creation needed to seed the database with its default values.
@@ -43,6 +43,7 @@ client9 = Client.create!(first_name: "Lucie", last_name:"Azquia", email:"lucie@g
 # product1 = Product.new(name:"",description:"", units_price_cents: ,unit_type:"",product_fruit: "", product_type:"", product_category:"")
 # product1.photo.attach(io: File.open(Rails.root.join('db/fixtures/')), filename: '.png', content_type: 'image/png')
 
+#création de seulement 4 produits pour réduire le temps de chargement du seed lors d'essai et le nombre d'images stockés sur cloudinary
 product1 = Product.new(name:"Cerises",description:"Cerises en sirop", unit_price_cents: 800, unit_type:"pot", product_fruit: "", product_type:"", product_category:"")
 product1.photo.attach(io: File.open(Rails.root.join('db/fixtures/products/cerises.jpg')), filename: 'cerises.jpg', content_type: 'image/jpg')
 product1.save!
@@ -59,18 +60,28 @@ product4 = Product.new(name:"Compote",description:"Compotte en pots", unit_price
 product4.photo.attach(io: File.open(Rails.root.join('db/fixtures/products/compote.jpg')), filename: 'compote.jpg', content_type: 'image/jpg')
 product4.save!
 
-# 5 - Create "order"
+# 4 - Create "order"
+#création de commandes "completed" pour calcul statistique
 order1 = Order.create!(client_id: client2.id ,date: Date.new(2020,4,14) ,total_price_cents: 6000 ,status:"completed", payment_method:"cash")
+order2 = Order.create!(client_id: client3.id ,date: Date.new(2020,4,12) ,total_price_cents: 10000 ,status:"completed", payment_method:"cash")
 
-# 4 - Create "order_lines"
+# 5 - Create "order_lines"
 order_line1 = OrderLine.create!(order_id: order1.id ,product_id: product2.id, quantity: 5 ,total_price_cents: 6000)
+#création d'1 order avec 3 order_line
+order_line2 = OrderLine.create!(order_id: order2.id ,product_id: product2.id, quantity: 10 ,total_price_cents: 12000)
+order_line3 = OrderLine.create!(order_id: order2.id ,product_id: product3.id, quantity: 2 ,total_price_cents: 1400)
+order_line4 = OrderLine.create!(order_id: order2.id ,product_id: product4.id, quantity: 4 ,total_price_cents: 2000)
+
+# 6 - create "product_lots"
+# création d'un lot pour chaque produit en stock (cf les 4 produits)
+product_lot1 = ProductLot.create!(product_id: product1.id ,production_date: Date.new(2020,3,1) ,expiry_date: Date.new(2020,5,1), quantity: 50, remaining_quantity: 45, lot_number: "lot 1")
+product_lot2 = ProductLot.create!(product_id: product2.id ,production_date: Date.new(2020,3,15) ,expiry_date: Date.new(2020,5,1), quantity: 15, remaining_quantity: 10, lot_number: "lot 2")
+product_lot3 = ProductLot.create!(product_id: product3.id ,production_date: Date.new(2020,3,20) ,expiry_date: Date.new(2020,5,1), quantity: 10, remaining_quantity: 8, lot_number: "lot 3")
+product_lot4 = ProductLot.create!(product_id: product4.id ,production_date: Date.new(2020,3,25) ,expiry_date: Date.new(2020,4,20), quantity: 15, remaining_quantity: 13, lot_number: "lot 4")
 
 
-
-#6 - create "product_lots"
-
-product_lot1 = ProductLot.create!(product_id: product2.id ,production_date: Date.new(2020,3,1) ,expiry_date: Date.new(2020,5,1), quantity: 50, remaining_quantity: 45, lot_number: "first - essai")
 # 7 - create "order_line_product_lots"
-
 order_line_product_lot1 = OrderLineProductLot.create!(order_line_id: order_line1.id ,product_lot_id: product_lot1.id ,quantity: 5)
-
+order_line_product_lot2 = OrderLineProductLot.create!(order_line_id: order_line2.id ,product_lot_id: product_lot2.id ,quantity: 5)
+order_line_product_lot3 = OrderLineProductLot.create!(order_line_id: order_line3.id ,product_lot_id: product_lot3.id ,quantity: 2)
+order_line_product_lot4 = OrderLineProductLot.create!(order_line_id: order_line4.id ,product_lot_id: product_lot4.id ,quantity: 2)
