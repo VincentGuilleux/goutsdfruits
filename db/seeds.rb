9143423071c1e809e5f6bd9a4ad45137b6efc56e
sebastien etienne <sebastienetienne.pro@gmail.com>
2020-04-14 15:49:16 +0200

    clients done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,3 @@
-
-
 # require "open-uri"
 
 Client.destroy_all
@@ -29,5 +27,18 @@ Product_lot.destroy_all
 
 
 # 1 - Create "client"
+client1 = Client.create(first_name: "Sébastien", last_name:"Etienne", email:"sebastien@gmail.com", phone:"0654789654", address:"Le Plessis",post_code:"79140", city:"Cerizay")
+client2 = Client.create(first_name: "Anne", last_name:"Mabire", email:"anne@gmail.com", phone:"", address:"",post_code:"", city:"")
+client3 = Client.create(first_name: "Maud", last_name:"De La Gourmerie", email:"", phone:"", address:"",post_code:"", city:"")
+client4 = Client.create(first_name: "Vincent", last_name:"GUilleux", email:"", phone:"", address:"",post_code:"", city:"")
+client5 = Client.create(first_name: "", last_name:"", email:"", phone:"", address:"",post_code:"", city:"")
+client6 = Client.create(first_name: "", last_name:"", email:"", phone:"", address:"",post_code:"", city:"")
+client7 = Client.create(first_name: "", last_name:"", email:"", phone:"", address:"",post_code:"", city:"")
+client8 = Client.create(first_name: "", last_name:"", email:"", phone:"", address:"",post_code:"", city:"")
+client9 = Client.create(first_name: "", last_name:"", email:"", phone:"", address:"",post_code:"", city:"")
+client9 = Client.create(first_name: "", last_name:"", email:"", phone:"", address:"",post_code:"", city:"")
 # 2 - create "product"
+
+product1 = Product.new(name:"",description:"", units_price_cents: ,)
+
 # 3 - Create "order"
