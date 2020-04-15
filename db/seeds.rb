51d555cf7748dd63318bb7dca4201fd062a8ba28
sebastien etienne <sebastienetienne.pro@gmail.com>
2020-04-15 18:50:04 +0200

    seed second batch

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,7 +29,7 @@ user_admin = User.create!(email: 'marie@gmail.com', password: 'azerty', ) #name:
 user_admin.avatar.attach(io: File.open(Rails.root.join('db/fixtures/users/madame-gazeau.jpg')), filename: 'madame-gazeau.jpg', content_type: 'image/jpg')
 user_admin.save!
 
-puts "user done"
+puts "      |    /|     - user done"
 
 # 1 - Create "client"
 client1 = Client.create!(first_name: "Sébastien", last_name:"Etienne", email:"sebastien@gmail.com", phone:"0654789654", address:"Le Plessis",post_code:"79140", city:"Cerizay")
@@ -44,7 +44,7 @@ client9 = Client.create!(first_name: "Gad", last_name:"Elmaleh", email:"gad@gmai
 client9 = Client.create!(first_name: "Mélanie", last_name:"Bouldu", email:"mélanie@gmail.com", phone:"0685976433", address:"rue du disc-jokey",post_code:"79530", city:"Brest")
 client9 = Client.create!(first_name: "Lucie", last_name:"Azquia", email:"lucie@gmail.com", phone:"0674548599", address:"rue des prés",post_code:"79100", city:"Breux")
 
-puts "client done"
+puts "       )  ( ') - client done"
 
 # 2 - create "product"
 
@@ -86,7 +86,7 @@ product5 = Product.new(name:"Gelée de pommes / sureau",description:"Gelée de p
 product5.photo.attach(io: File.open(Rails.root.join('db/fixtures/products/gelée.jpg')), filename: 'gelée.jpg', content_type: 'image/jpg')
 product5.save!
 
-puts "product 1/5"
+puts "      (  /  )  - product 1/5"
 product6 = Product.new(name:"Gelée de groseilles",description:"Gelée de groseilles", unit_price_cents: 600 ,unit_type:"pot",product_fruit: "groseilles", product_type:"gelée", product_category:"gourmandise")
 product6.photo.attach(io: File.open(Rails.root.join('db/fixtures/products/gelée.jpg')), filename: 'gelée.jpg', content_type: 'image/jpg')
 product6.save!
@@ -108,8 +108,7 @@ product9.save!
 product10 = Product.new(name:"Sirop de sureau",description:"Sirop de sureau", unit_price_cents: 700, unit_type:"bouteille", product_fruit: "sureau", product_type:"sirop", product_category:"gourmandise")
 product10.photo.attach(io: File.open(Rails.root.join('db/fixtures/products/sirop.jpg')), filename: 'sirop.jpg', content_type: 'image/jpg')
 product10.save!
-
-puts "product 2/5"
+puts "       |(__)|   - product 2/5"
 
 product11 = Product.new(name:"Sirop de fraise",description:"Sirop de fraise", unit_price_cents: 700, unit_type:"bouteille", product_fruit: "fraise", product_type:"sirop", product_category:"gourmandise")
 product11.photo.attach(io: File.open(Rails.root.join('db/fixtures/products/sirop.jpg')), filename: 'sirop.jpg', content_type: 'image/jpg')
@@ -133,8 +132,7 @@ product14.save!
 product15 = Product.new(name:"Coulis de câssis",description:"Coulis de câssis", unit_price_cents: 800 ,unit_type:"pot",product_fruit: "câssis", product_type:"coulis", product_category:"gourmandise")
 product15.photo.attach(io: File.open(Rails.root.join('db/fixtures/products/coulis.jpg')), filename: 'coulis.jpg', content_type: 'image/jpg')
 product15.save!
-
-puts "product 3/5"
+puts "cat's team     - product 3/5"
 
 product16 = Product.new(name:"Coulis de mûres sauvages",description:"Coulis de mûres sauvages", unit_price_cents: 800 ,unit_type:"pot",product_fruit: "mûres sauvages", product_type:"coulis", product_category:"gourmandise")
 product16.photo.attach(io: File.open(Rails.root.join('db/fixtures/products/coulis.jpg')), filename: 'coulis.jpg', content_type: 'image/jpg')
@@ -161,13 +159,11 @@ product19.save!
 product20 = Product.new(name:"Fraises fraîches en parquettes",description:"Fraises fraîches en parquettes", unit_price_cents: 600 ,unit_type:"gramme",product_fruit: "fraise", product_type:"fruit", product_category:"fruit frais")
 product20.photo.attach(io: File.open(Rails.root.join('db/fixtures/products/fraises.jpg')), filename: 'fraises.jpg', content_type: 'image/jpg')
 product20.save!
-
 puts "product 4/5"
 
 product21 = Product.new(name:"Prunes fraîches aux Kgs",description:"Prunes fraîches en parquettes", unit_price_cents: 600 ,unit_type:"gramme",product_fruit: "prune", product_type:"fruit", product_category:"fruit frais")
 product21.photo.attach(io: File.open(Rails.root.join('db/fixtures/products/fraises.jpg')), filename: 'fraises.jpg', content_type: 'image/jpg')
 product21.save!
-
 
 #les plants
 
@@ -180,7 +176,6 @@ product22.save!
 product23 = Product.new(name:"Plant de pomme",description:"Plant de pomme", unit_price_cents: 90000 ,unit_type:"plant",product_fruit: "pomme", product_type:"végétal", product_category:"Pépinière")
 product23.photo.attach(io: File.open(Rails.root.join('db/fixtures/products/grand-plant.jpg')), filename: 'grand-plant.jpg', content_type: 'image/jpg')
 product23.save!
-
 puts "product done"
 
 
@@ -192,7 +187,7 @@ order2 = Order.create!(client_id: client3.id ,date: Date.new(2020,4,12) ,total_p
 order3 = Order.create!(client_id: client4.id ,date: Date.new(2020,4,15) ,total_price_cents: 16200 ,status:"pending", payment_method:"cash")
 order4 = Order.create!(client_id: client5.id ,date: Date.new(2020,4,10) ,total_price_cents: 3200 ,status:"prepared", payment_method:"card")
 order5 = Order.create!(client_id: client6.id ,date: Date.new(2020,4,8) ,total_price_cents: 3200 ,status:"delivered", payment_method:"card")
-
+order6 = Order.create!(client_id: client7.id ,date: Date.new(2020,4,10) ,total_price_cents: 1400 ,status:"pending", payment_method:"card")
 puts "order done"
 
 # 5 - Create "order_lines" #order1
@@ -212,11 +207,12 @@ order_line8 = OrderLine.create!(order_id: order3.id ,product_id: product12.id, q
 order_line9 = OrderLine.create!(order_id: order4.id ,product_id: product1.id, quantity: 4 ,total_price_cents: 3200)
 # création d'un order delivered
 order_line10 = OrderLine.create!(order_id: order5.id ,product_id: product17.id, quantity: 2 ,total_price_cents: 1600)
-order_line10 = OrderLine.create!(order_id: order5.id ,product_id: product18.id, quantity: 2 ,total_price_cents: 1600)
+order_line11 = OrderLine.create!(order_id: order5.id ,product_id: product18.id, quantity: 2 ,total_price_cents: 1600)
+
+order_line12 = OrderLine.create!(order_id: order6.id ,product_id: product10.id, quantity: 2 ,total_price_cents: 1400)
+puts "order_line done"
 
 
-
-puts "order-line done"
 # 6 - create "product_lots"
 # création d'un lot pour chaque produit en stock (cf les 4 produits)
 product_lot1 = ProductLot.create!(product_id: product1.id ,production_date: Date.new(2020,3,1) ,expiry_date: Date.new(2020,5,1), quantity: 50, remaining_quantity: 45, lot_number: "lot 1")
@@ -228,13 +224,20 @@ product_lot4 = ProductLot.create!(product_id: product4.id ,production_date: Date
 product_lot5 = ProductLot.create!(product_id: product5.id ,production_date: Date.new(2020,2,25) ,expiry_date: Date.new(2020,6,20), quantity: 50, remaining_quantity: 0, lot_number: "lot 5")
 product_lot6 = ProductLot.create!(product_id: product6.id ,production_date: Date.new(2020,2,25) ,expiry_date: Date.new(2020,6,20), quantity: 50, remaining_quantity: 0, lot_number: "lot 6")
 
-
-puts "product lot done"
+#lot avec date de préremption inf à 15 jrs
+product_lot7 = ProductLot.create!(product_id: product7.id ,production_date: Date.new(2019,2,25) ,expiry_date: Date.new(2020,4,25), quantity: 50, remaining_quantity: 10, lot_number: "lot 5")
+product_lot8 = ProductLot.create!(product_id: product8.id ,production_date: Date.new(2019,2,25) ,expiry_date: Date.new(2020,4,29), quantity: 50, remaining_quantity: 5, lot_number: "lot 6")
+puts "product_lot done"
 
 # 7 - create "order_line_product_lots"
 order_line_product_lot1 = OrderLineProductLot.create!(order_line_id: order_line1.id ,product_lot_id: product_lot1.id ,quantity: 5)
 order_line_product_lot2 = OrderLineProductLot.create!(order_line_id: order_line2.id ,product_lot_id: product_lot2.id ,quantity: 5)
 order_line_product_lot3 = OrderLineProductLot.create!(order_line_id: order_line3.id ,product_lot_id: product_lot3.id ,quantity: 2)
 order_line_product_lot4 = OrderLineProductLot.create!(order_line_id: order_line4.id ,product_lot_id: product_lot4.id ,quantity: 2)
-
-puts " order line product done"
+puts "order_line_product_done"
+puts""
+puts"made with love by the cat's team"
+puts""
+puts"               )|._.,--....,'``.       "
+puts" .b--.        /;   _.. |   _|  (`._ ,. "
+puts"`=,-,-'~~~   `----(,_..'--(,_..'`-.;.'"
