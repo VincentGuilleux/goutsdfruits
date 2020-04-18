68bcd0073bf6831bdba23031861e6fd3215b8998
Anne Mabire <a.b.m.mabire@gmail.com>
2020-04-18 18:29:02 +0200

    modifications svg to fontawesome

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,11 +15,24 @@ class OrdersController < ApplicationController
   def deliver
     @order = Order.find(params[:id])
     if @order.status == "prepared"
-      @order.status = "deliver"
+      @order.status = "delivered"
       @order.save!
       redirect_to dashboard_path
     else
       @order.status = "prepared"
+      @order.save!
+      redirect_to dashboard_path
+    end
+  end
+
+  def pay
+    @order = Order.find(params[:id])
+    if @order.status == "delivered"
+      @order.status = "paied"
+      @order.save!
+      redirect_to dashboard_path
+    else
+      @order.status = "delivered"
       @order.save!
       redirect_to dashboard_path
     end
