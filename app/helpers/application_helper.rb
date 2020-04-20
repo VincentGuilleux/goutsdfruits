207596b3fb2b777606d2e261c1f70ef392849a52
Vincent Guilleux <vincent.guilleux@yahoo.fr>
2020-04-20 10:59:50 +0200

    update index view + i18n for french/eng

--- app/helpers/application_helper.rb
+++
@@ -1,6 +0,0 @@
-module ApplicationHelper
-
- # def pluralize(count, singular, plural = nil)
- #    "#{count || 0} " + ([0, 1, '0', '1'].include?(count) ? singular : (plural || singular.pluralize))
- #  end
-end
