34abd47af6830e943fa6813af5d2f48c3cc3ea61
Vincent Guilleux <vincent.guilleux@yahoo.fr>
2020-04-20 22:07:36 +0200

    fixseeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,13 @@
  require "open-uri"
 
-# OrderLineProductLot.destroy_all
-# ProductLot.destroy_all
-# OrderLine.destroy_all
-# Order.destroy_all
-# Client.destroy_all
-# Product.destroy_all
-# User.destroy_all
-rails db:drop
-rails db:create
-rails db:migrate
-rails db:seed
+OrderLineProductLot.destroy_all
+ProductLot.destroy_all
+OrderLine.destroy_all
+Order.destroy_all
+Client.destroy_all
+Product.destroy_all
+User.destroy_all
+
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
