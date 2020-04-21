1b2703a5c2af0a2206b5b452d5e58eb7f4bac870
Anne Mabire <a.b.m.mabire@gmail.com>
2020-04-21 13:18:38 +0200

    une commande peut être créée mais le décompte sur les quantité des lots n'est pas encore finctionnelle

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -46,6 +46,8 @@ class OrdersController < ApplicationController
   def create
     @order = Order.new(order_params)
 
+    @order.date = Date.today
+
     sum = 0
 
     @order.order_lines.each do |order_line|
@@ -56,6 +58,7 @@ class OrdersController < ApplicationController
     @order.total_price_cents = sum
 
     if @order.save!
+      # generate_junction
       redirect_to dashboard_path
     else
       render :new
@@ -67,4 +70,24 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order).permit(:client_id, order_lines_attributes: [:product_id, :quantity])
   end
+
+  # def generate_junction
+  #   @order.order_lines.each do |order_line|
+  #     @order_line_product_lot = OrderLineProductLot.new
+  #     @order_line_product_lot.quantity = order_line.quantity
+  #     @order_line_product_lot.order_line_id = order_line.id
+
+  #     if order_line.product.product_lots.first.remaining_quantity >= order_line.quantity
+  #       @order_line_product_lot.product_lot_id = order_line.product.product_lots.first.id
+  #       order_line.product_lots.first.remaining_quantity -= order_line.quantity
+  #     else
+  #       @order_line_product_lot.product_lot_id = order_line.product.product_lots.first.id
+  #       @order_line_product_lot.quantity = order_line.product_lots.first.remaining_quantity
+  #       @order_line_product_lot = OrderLineProductLot.new
+        # end
+
+
+  #     binding.pry
+  #   end
+  # end
 end
