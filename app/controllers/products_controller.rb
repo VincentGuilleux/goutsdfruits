be0a8c62729eed42f3ec596d7c505921d5812755
Vincent Guilleux <vincent.guilleux@yahoo.fr>
2020-04-21 17:30:46 +0200

    filterproductindexok

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,21 @@
 class ProductsController < ApplicationController
 
   def index
-    @products = Product.all.sort_by do |product|
-      product.total_remaining_quantity
+      @products = Product.all
+    if params[:fruit].present?
+      @products = @products.where(product_fruit: params[:fruit])
     end
+    if params[:category].present?
+      @products = @products.where(product_category: params[:category])
+    end
+    if params[:type].present?
+      @products = @products.where(product_type: params[:type])
+    end
+      # on peut cumuler des requetes Active Record (cf. plus haut) car elles ne sont pas appliquées tant qu'on ne fait pas un each ou un sort dessus (cf. ligne plus bas)
+      @products = @products.sort_by do |product|
+        product.total_remaining_quantity
+      end
+
   end
 
   def show
