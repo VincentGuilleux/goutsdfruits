394857e53782bbefbf0b8836667bf712b485fe1f
Maud de La Gournerie mdelagournerie@live.fr <mdelagournerie@live.fr>
2020-04-23 12:44:43 +0200

    chart intégré

--- app/javascript/components/initchart.js
+++ app/javascript/components/initchart.js
@@ -1,41 +1,45 @@
 import Chart from "chart.js"
-​
+
 let chart;
-​
+
 export const InitChart = () => {
   // destroy previous chart if present
   if (chart) {
     chart.destroy();
     chart = null;
   }
-​
+
   const container = document.getElementById('chartContainer');
-  if (!container) return
-​
+  if (!container) return;
+
+
+
+  // container.style.width  = `${container.parentElement.clientWidth}px`;
+  // container.style.height = `${container.parentElement.clientHeight}px`;
+  container.innerHTML = `<canvas id="vente_chart"</canvas>`;
+
   setTimeout(() => {
     const ctx = document.getElementById('vente_chart').getContext('2d');
-​
+
     const chart = new Chart(ctx, {
       // The type of chart we want to create
       type: 'line',
-​
+
       // The data for our dataset
       data: {
-        labels: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin'],
+        labels: ['Janvier', 'Février', 'Mars', 'Avril'],
         datasets: [
           {
             label: '2019',
-            backgroundColor: 'transparent',
             borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30],
+            data: [0, 10, 5, 2],
             fill: false,
             lineTension: 0
           },
           {
             label: '2020',
-            backgroundColor: 'transparent',
             borderColor: 'rgb(255, 255, 132)',
-            data: [0, 4, 8, 2],
+            data: [0, 4, 8, 3],
             fill: false,
             lineTension: 0
           }
