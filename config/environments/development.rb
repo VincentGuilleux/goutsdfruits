c27f1f1e02ff345b84f3884e9a91ec32d9604d9c
Vincent Guilleux <vincent.guilleux@yahoo.fr>
2020-05-04 10:18:40 +0200

    bullet&rakeminiprofilerinstall

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,4 +60,10 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.after_initialize do
+  Bullet.enable = true
+  Bullet.rails_logger = true
+  Bullet.slack = { webhook_url: 'http://lewagon-alumni.slack.com', channel: '#goutdfruits_no-teachers', username: 'notifier' }
+end
 end
