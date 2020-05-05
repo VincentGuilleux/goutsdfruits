f71714c7362c9d202426e9dee7d91286c8f822ee
Vincent Guilleux <vincent.guilleux@yahoo.fr>
2020-05-05 12:39:47 +0200

    form new product finished

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,4 +25,25 @@ class ProductsController < ApplicationController
     @product_lot = ProductLot.new
   end
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save!
+      redirect_to products_path
+    else
+      # flash[:alert] = "Tous les champs doivent être saisis"
+      render :new
+      # return
+    end
+  end
+
+     private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :unit_price_cents, :unit_type, :product_fruit, :product_type, :product_category, :photo)
+  end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,12 @@ class Product < ApplicationRecord
   has_many :order_lines
   has_many :orders, through: :order_lines #allows to find all orders for a given product
   has_one_attached :photo
+  validates :name, uniqueness: true
+  validates :unit_price_cents, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :unit_type, presence: true
+  validates :product_type, presence: true
+  validates :product_fruit, presence: true
+  validates :product_category, presence: true
 
   def total_remaining_quantity
     total_remaining_quantity = 0
@@ -12,9 +18,13 @@ class Product < ApplicationRecord
     return total_remaining_quantity
   end
 
-  # les méthodes ci-dessous permettent de mapper pour chaque produit son product_type. Les doublons sont ensuite éliminés grâce au .uniq.
+  # les méthodes ci-dessous permettent de mapper pour chaque produit type/category/fruit. Les doublons sont ensuite éliminés grâce au .uniq.
   # &:product_type est un raccourci syntaxtique qui correspond à : Product.all.map do |product| product.fruit_product
   # LEFT TO DO: GERER LE CAPITALIZE POUR AVOIR LES CHAMPS CAPITALIZES EN VISU map(&:capitalize)
+
+  def self.units
+    Product.all.map(&:unit_type).uniq.sort_by { |word| word.downcase }
+  end
 
   def self.types
     Product.all.map(&:product_type).uniq.sort_by { |word| word.downcase }
