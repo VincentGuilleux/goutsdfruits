0c0f6c05f2571175453c59ee913191f2e51eaf6f
Vincent Guilleux <vincent.guilleux@yahoo.fr>
2020-05-05 10:05:36 +0200

    routes updated and button new added in index

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,11 @@ Rails.application.routes.draw do
 
   resources :clients, only: [:index] #Not nested because not linked to a client show (the client will be input in the form)
 
-  resources :products, only: [:index, :show] do
+  resources :products, only: [:index, :show, :new, :create] do
     resources :product_lots, only: [:create] # has many between orders & product_lots ? Not necessary
   end
 
-  resources :orders, only: [:index, :new, :create] do # No show because collapse
+  resources :orders, only: [:index, :new, :create] do # No show because collapse (no specific URL)
     member do
       patch :prepare
       patch :deliver
