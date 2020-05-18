85ed306062f288ed43ea1b577033477cfe2693f3
Anne Mabire <a.b.m.mabire@gmail.com>
2020-05-18 16:43:37 +0200

    delivery date and place added to index

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :client
+  belongs_to :delivery_place
   has_many :order_lines, dependent: :destroy
   has_many :products, through: :order_lines
   validates :total_price_cents, presence: true, numericality: { only_integer: true, greater_than: 0 }
