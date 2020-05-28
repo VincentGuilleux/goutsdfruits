3a988d3b5c48e738d8a0ded4eba6347eaa474be4
Vincent Guilleux <vincent.guilleux@yahoo.fr>
2020-05-28 21:26:55 +0200

    devise config ok for new registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_client!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
-  end
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :phone, :address, :post_code, :city, :segment, :amap])
   end
 
   private
@@ -22,7 +15,11 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  protected
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :phone, :address, :post_code, :city, :segment, :amap])
+  end
 
 end
 
