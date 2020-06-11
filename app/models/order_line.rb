baef8699e4517963a3743c1995910d7291be7ccb
Vincent Guilleux <vincent.guilleux@yahoo.fr>
2020-06-11 12:08:11 +0200

    add dependend destroy between order_line & order_line_product_lot

--- app/models/order_line.rb
+++ app/models/order_line.rb
@@ -1,7 +1,7 @@
 class OrderLine < ApplicationRecord
   belongs_to :order
   belongs_to :product
-  has_many :order_line_product_lots
+  has_many :order_line_product_lots, dependent: :destroy
   has_many :product_lots, through: :order_line_product_lots
   validates :total_price_cents, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
