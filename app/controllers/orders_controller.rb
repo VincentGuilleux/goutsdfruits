3b4495834e47c6ca2908601a7ceeb232d4473962
Vincent Guilleux <vincent.guilleux@yahoo.fr>
2020-06-12 12:25:12 +0200

    delete method order added

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -135,7 +135,8 @@ class OrdersController < ApplicationController
   end
 
   def destroy
-    order.destroy
+    @order = Order.find(params[:id])
+    @order.destroy
     redirect_to orders_path
   end
 
