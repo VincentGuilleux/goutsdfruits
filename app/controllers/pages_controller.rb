96ffb8bf512b0dd83764fc126d7cb032d54b3ccc
Vincent Guilleux <vincent.guilleux@yahoo.fr>
2020-06-29 14:30:18 +0200

    flash notice on contact form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,6 +10,8 @@ class PagesController < ApplicationController
 
   def send_contact
      ContactMailer.contact(params[:message], params[:email]).deliver
+     flash[:notice] = "Votre message a bien été envoyé !"
+     redirect_back(fallback_location: root_path) # les messages flash Rails ne s'affichent qu'après rechargement de la page
   end
 
 end
