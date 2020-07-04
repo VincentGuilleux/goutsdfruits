13e5773fdc42182b3942a5c2ca47c3354fbf6af9
Vincent Guilleux <vincent.guilleux@yahoo.fr>
2020-07-04 17:59:18 +0200

    mail notification added when new order + refacto orders_controller

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,7 +1,11 @@
 class OrderMailer < ApplicationMailer
 
- def new_order_email(client)
+  def new_order_email(client)
     mail(to: client.email, subject: 'Commande GoutsdFruits')
+  end
+
+  def new_order_email_notification(client)
+    mail(to: 'goutsdfruits@gmail.com', subject: "Nouvelle commande passée par #{client.first_name} #{client.last_name}")
   end
 
 end
