5990adce4fef9e64a0ad39354fc0d215f0cd81c9
Vincent Guilleux <vincent.guilleux@yahoo.fr>
2020-07-08 09:40:49 +0200

    fix orders dropdown not working

--- app/javascript/components/dropdown.js
+++ app/javascript/components/dropdown.js
@@ -1,5 +1,4 @@
 // DROPDOWNS PRODUCT INDEX
-// Les méthodes ci-dessous écoutent les clicks sur chacun des dropdown menus et renvoient la valeur cliquée
 
 const getdropdownids = () => {
   const product_fruit = document.getElementById("product_fruit"); // target l'id du champ fruit (le "titre" du Dropdown Menu)
@@ -113,54 +112,8 @@ export { dropdownType } ;
 export { dropdownPrice } ;
 export { clearFilters } ;
 
-// CODE SUPPRIME : CODE V1 QUI RECHARGAIT LA PAGE, CHAQUE FONCTION DROPDOWN LANCAIT A LA FIN L'URL LA FONTION URL_PATH CI CI-DESSOUS QUI VENAIT CONSTRUIRE L'URL DE RECHERCHE ET RECHARGER LA PAGE EN FONCTION
-// const url_path = () => {
-// const url = window.location.origin + window.location.pathname + "?" + fruitRequest() + categoryRequest() + typeRequest() + priceRequest() ;
-// window.location.href = url;
-// https://stackoverflow.com/questions/47583856/window-location-href-vs-history-pushstate-which-to-use#:~:text=location.,the%20other%20hand%2C%20what%20history.&text=pushState()%20changes%20the%20referrer,after%20you%20change%20the%20state.
-// };
-
-// const fruitRequest = () => {
-// let fruitQuery ="";
-// const product_fruit = document.getElementById("product_fruit");
-//     if (product_fruit.innerText !== 'Fruit') {
-//       fruitQuery = "&fruit=" + product_fruit.innerText
-//     };
-// return fruitQuery;
-// };
-
-// const categoryRequest = () => {
-// let catQuery ="";
-// const product_category = document.getElementById("product_category");
-//     if (product_category.innerText !== 'Catégorie') {
-//       catQuery = "&category=" + product_category.innerText
-//     };
-// return catQuery;
-// };
-
-// const typeRequest = () => {
-// let typeQuery ="";
-// const product_type = document.getElementById("product_type");
-//     if (product_type.innerText !== 'Type') {
-//       typeQuery = "&type=" + product_type.innerText
-//     };
-// return typeQuery;
-// };
-
-// const priceRequest = () => {
-// let priceQuery ="";
-// const product_price = document.getElementById("product_price");
-//   if (product_price !== null) { // condition nécessaire car pour les non-admin le priceRequest n'apparait pas
-//     if (product_price.innerText !== 'Prix') {
-//       priceQuery = "&price=" + product_price.innerText
-//     };
-//   };
-// return priceQuery;
-// };
-
-// --------------------------------------------------------------------------------
-
 // DROPDOWNS CLIENT INDEX
+
 const dropdownSegment = () => {
   const segment = document.getElementById("DropdownSegment");
   if (!segment) return
@@ -211,12 +164,7 @@ window.location.href = url;
 export { dropdownSegment } ;
 export { dropdownAmap } ;
 
-
-// --------------------------------------------------------------------------------
-
 // DROPDOWNS ORDER INDEX
-
-// filtre segment
 
 const dropdownSegmentOrders = () => {
   const segmentOrders = document.getElementById("dropdownSegmentOrders");
