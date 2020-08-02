3946794809db1ed8bec6d535d1b6050ddab3a047
Vincent Guilleux <vincent.guilleux@yahoo.fr>
2020-08-02 14:17:51 +0200

    added delete client in clients index

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -19,6 +19,18 @@ class ClientsController < ApplicationController
     "#{first_name.capitalize} #{last_name.capitalize}"
   end
 
+  def destroy
+    @client = Client.find(params[:id])
+    unless @client.orders.nil?
+      flash.keep[:alert] = "Le client sélectionné a déjà passé des commandes, il ne peut pas être supprimé."
+      redirect_to clients_path
+      return
+    end
+    @client.destroy
+    flash[:notice] = "Le client a été supprimé."
+    redirect_to clients_path
+  end
+
   private
 
   def client_params
