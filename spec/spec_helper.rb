544c12c638fd3cee1b8f56a983f5904f0670b43d
Vincent Guilleux <vincent.guilleux@yahoo.fr>
2020-09-17 10:21:36 +0200

    dev seeds autoload in test db

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,7 +15,7 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   config.before(:suite) do
-    Rails.application.load_seed # loading seeds
+    Rails.application.load_seed # loads seeds from seeds.rb each time rspec is run
   end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
