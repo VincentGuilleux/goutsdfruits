ebbab9eca6b0b2c4e803da3a9396840a07db53ff
Vincent Guilleux <vincent.guilleux@yahoo.fr>
2020-09-22 12:54:45 +0200

    add unit test for total_remaining_quantity & fix product model

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,5 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-require 'database_cleaner/active_record'
 
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
